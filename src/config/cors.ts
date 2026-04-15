24c0567b5e52eea0318b9204248726a9ca054b7e
habiburRhaman05 <devhabib2005@gmail.com>
2026-04-15 22:46:21 +0600

    add cors and tsup settings

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,5 +1,5 @@
 export const corsConfig = {
-  origin: ['http://localhost:3000',"https://skill-bridge-frontend-gamma.vercel.app",'https://skill-bridge-frontend-4vp4.vercel.app'],
+  origin: ['http://localhost:3000',"https://nex-leve-academy.vercel.app"],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS','PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
   credentials: true,
